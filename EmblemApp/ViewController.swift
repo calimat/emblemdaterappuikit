eb67a9c836fa3ce8f24e339ac616346682f541ed
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-10-11 16:10:49 -0500

    Initial Commit

---
+++ EmblemApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmblemApp
+//
+//  Created by Ricardo Herrera Petit on 10/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
