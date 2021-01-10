5493a80d5c550cb5005b142d3d72cc93b46b52df
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2021-01-09 19:08:30 -0500

    Created a test target for the Emblem File Mapper tests.

---
+++ EmblemFileMapperTests/EmblemFileMapperTests.swift
@@ -0,0 +1,16 @@
+//
+//  EmblemFileMapperTests.swift
+//  EmblemFileMapperTests
+//
+//  Created by Ricardo Herrera Petit on 1/9/21.
+//
+
+import XCTest
+
+class EmblemFileMapperTests: XCTestCase {
+
+    func test_donothing() {
+        
+    }
+
+}
